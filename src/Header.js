ebc39dd1e039585ed6a22664254cbbe93c600a5b
Tiegow <tiegorafael@gmail.com>
2021-12-06 19:30:07 -0300

    Criando aplicação React

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <div className="Header">
+      <header>
+          <img src="https://logospng.org/download/netflix/logo-netflix-2048.png"/> 
+          <a href=""><button>Entrar</button></a>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
